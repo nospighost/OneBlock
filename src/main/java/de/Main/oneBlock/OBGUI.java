730699535610c8edce3fefa256a590d540c5b026
nospighost <noel.spieker@icloud.com>
2025-05-12 19:16:05 +0200

    Oneblock | 16

---
+++ src/main/java/de/Main/oneBlock/OBGUI.java
@@ -0,0 +1,13 @@
+package de.Main.OneBlock;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class OBGUI implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        System.out.println("Test23423423");
+        return false;
+    }
+}
