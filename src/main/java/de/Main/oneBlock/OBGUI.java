ad0e22485de2ed977cdbac486f9d4ac9ff6d365b
nospighost <noel.spieker@icloud.com>
2025-05-12 19:22:02 +0200

    Oneblock | 19

--- src/main/java/de/Main/oneBlock/OBGUI.java
+++ src/main/java/de/Main/oneBlock/OBGUI.java
@@ -1,16 +1,19 @@
 package de.Main.OneBlock;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class OBGUI implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         Player player = (Player) commandSender;
-        player.sendMessage("Test");
-        System.out.println("Test");
+        Inventory MainGUI = Bukkit.createInventory(null, 8 * 8, "OneBlock");
+        player.openInventory(MainGUI);
+
         return false;
     }
 }
