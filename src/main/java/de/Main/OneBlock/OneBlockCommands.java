ba49b37311b92acd0ddb68fccb059dcfb8b83685
Fabi <Fabghost@gmail.com>
2025-05-15 17:53:04 +0200

    Oneblock 0.0.9 | Merge

--- src/main/java/de/Main/OneBlock/OneBlockCommands.java
+++ src/main/java/de/Main/OneBlock/OneBlockCommands.java
@@ -29,8 +29,8 @@ public class OneBlockCommands implements Listener, CommandExecutor {
         } else if (args.length == 1 && args[0].equalsIgnoreCase("delete")) {
             Manager.deleteIsland(player);
         }else if (args.length == 2 && args[0].equalsIgnoreCase("visit")) {
-                String targetName = args[1];
-                Manager.visitIsland(player, targetName);
+            String targetName = args[1];
+            Manager.visitIsland(player, targetName);
         } else if (args.length == 1 && args[0].equalsIgnoreCase("rebirth")) {
 
             if (config.getInt("IslandLevel")== 4 && (player.getInventory().firstEmpty() == -1)){  //a
@@ -43,12 +43,12 @@ public class OneBlockCommands implements Listener, CommandExecutor {
                 Manager.rebirthIsland(player);
             }
 
-            }
-         else {
-                player.sendMessage("Nutze: /ob join | /ob delete");
-            }
+        }
+        else {
+            player.sendMessage("Nutze: /ob join | /ob delete");
+        }
 
 
-            return true;
+        return true;
     }
 }
