70854ffce4366bbf302d3462e4f7556d2af8f1b8
nospighost <nospighost@gmail.com>
2025-06-13 15:55:42 +0200

    Oneblock 1.1.1 | Biom Switch

--- src/main/java/de/Main/OneBlock/WorldManager/WorldBorderManager.java
+++ src/main/java/de/Main/OneBlock/WorldManager/WorldBorderManager.java
@@ -6,9 +6,11 @@ import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.sql.Connection;
@@ -264,4 +266,8 @@ public class WorldBorderManager implements Listener {
         }
         return null;
     }
+
+
+
+
 }
